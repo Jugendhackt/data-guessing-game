0f7f4f3eaf54e9358315952d3bbefed8caeb9577
blinry <mail@blinry.org>
2022-08-13 16:02:42 +0200

    Parse all CSV files

--- scraper/src/index.ts
+++ scraper/src/index.ts
@@ -7,78 +7,75 @@ const diagrams = readdirSync('owid-datasets/datasets/', { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name)
 
-//diagrams.forEach(diagram => {
+const questions = []
 
-const diagram = "Whale catch by decade (Rocha et al. & IWC)"
+diagrams.forEach(diagram => {
+    //readFile(`owid-datasets/datasets/${diagram}/datapackage.json`, (_, data) => {
+    //    const datapackage = JSON.parse(data.toString())
+    //    console.log(datapackage)
+    //})
 
-//readFile(`owid-datasets/datasets/${diagram}/datapackage.json`, (_, data) => {
-//    const datapackage = JSON.parse(data.toString())
-//    console.log(datapackage)
-//})
+    const csvFiles = readdirSync(`owid-datasets/datasets/${diagram}`, { withFileTypes: true }).filter(fn => fn.name.endsWith(".csv"))
+    const csvFileName = `owid-datasets/datasets/${diagram}/${csvFiles[0].name}`
 
-const csvFiles = readdirSync(`owid-datasets/datasets/${diagram}`, { withFileTypes: true }).filter(fn => fn.name.endsWith(".csv"))
-const csvFileName = `owid-datasets/datasets/${diagram}/${csvFiles[0].name}`
+    type RawCSVLine = Record<string, string>
 
-type RawCSVLine = Record<string, string>
+    const results: Array<RawCSVLine> = []
 
-const results: Array<RawCSVLine> = []
+    createReadStream(csvFileName)
+      .pipe(csv())
+      .on('data', (data: RawCSVLine) => results.push(data))
+      .on('end', () => {
+        if (results.length > 0) {
+            let entities: Set<string> = new Set()
+            let columns: Array<string> = []
+            let minYear = 9999
+            let maxYear = -9999
 
-createReadStream(csvFileName)
-  .pipe(csv())
-  .on('data', (data: RawCSVLine) => results.push(data))
-  .on('end', () => {
-    console.log(results);
+            results.forEach(line => {
+                entities.add(line["Entity"])
+                const year = parseInt(line["Year"])
+                if (year > maxYear) {
+                    maxYear = year
+                }
+                if (year < minYear) {
+                    minYear = year
+                }
+            })
 
-    let entities: Set<string> = new Set()
-    let columns: Array<string> = []
-    let minYear = 9999
-    let maxYear = -9999
+            Object.keys(results[0]).filter(key => key !== "Entity" && key !== "Year").forEach(key => columns.push(key))
 
-    results.forEach(line => {
-        entities.add(line["Entity"])
-        const year = parseInt(line["Year"])
-        if (year > maxYear) {
-            maxYear = year
+            console.log(`${minYear} - ${maxYear}`)
+
+            const interestingColumn = columns[columns.length - 1]
+            let entity: string
+            if (entities.has("World")) {
+                entity = "World"
+            } else {
+                entity = Array.from(entities)[0]
+            }
+
+            let data = {
+                question: `${diagram} (${interestingColumn}, ${entity})`,
+                type: "series",
+                answer: [] as Array<{year: number, value: number}>,
+            }
+            results.forEach(line => {
+                if (line["Entity"] === entity) {
+                    const year = parseInt(line["Year"])
+                    const value = parseFloat(line[interestingColumn])
+                    const dataPoint = {year, value}
+                    data.answer.push(dataPoint)
+                }
+            })
+            console.log(data)
+            questions.push(data)
         }
-        if (year < minYear) {
-            minYear = year
-        }
-    })
+      });
 
-    Object.keys(results[0]).filter(key => key !== "Entity" && key !== "Year").forEach(key => columns.push(key))
+    //readFile(csvFileName, (_, data) => {
+    //    const content = data.toString()
+    //    console.log(.split('\n').length)
+    //})
 
-    console.log(entities)
-    console.log(columns)
-    console.log(`${minYear} - ${maxYear}`)
-
-    const interestingColumn = columns[columns.length - 1]
-    let entity: string
-    if (entities.has("World")) {
-        entity = "World"
-    } else {
-        entity = Array.from(entities)[0]
-    }
-
-    let data = {
-        question: `${diagram} (${interestingColumn}, ${entity})`,
-        type: "series",
-        answer: [] as Array<{year: number, value: number}>,
-    }
-    results.forEach(line => {
-        if (line["Entity"] === entity) {
-            const year = parseInt(line["Year"])
-            const value = parseFloat(line[interestingColumn])
-            const dataPoint = {year, value}
-            data.answer.push(dataPoint)
-        }
-    })
-    console.log(data)
-
-  });
-
-//readFile(csvFileName, (_, data) => {
-//    const content = data.toString()
-//    console.log(.split('\n').length)
-//})
-
-//})
+})
