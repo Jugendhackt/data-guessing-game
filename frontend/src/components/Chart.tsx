db83675c29f96e304d4dd323712c207cf80f1eab
blinry <mail@blinry.org>
2022-08-13 18:03:58 +0200

    Fix Chart component

--- frontend/src/components/Chart.tsx
+++ frontend/src/components/Chart.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useRef } from "react";
 import { Answer } from "../types";
 
-type Props = Answer
+type Props = {datapoints: Answer}
 
-export const Chart: React.FC<Props> = (datapoints) => {
+export const Chart: React.FC<Props> = ({datapoints}) => {
   const canvasRef = useRef(null);
   useEffect(() => {
     if (canvasRef === null) {
